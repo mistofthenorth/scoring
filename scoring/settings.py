fbf24d7ed1bb20d8b93a3c5d4982b9ff885c56f8
Brian Owen <briano.bend@icloud.com>
2022-02-08 21:21:44 -0800

    Updating db name

--- scoring/settings.py
+++ scoring/settings.py
@@ -97,7 +97,7 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
+            'NAME': ('postgres'),
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
