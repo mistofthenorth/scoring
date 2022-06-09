7aa92ede8c086f34f69e2a6ea723e4abbfd12f00
Brian Owen <briano.bend@icloud.com>
2022-06-08 22:38:39 -0700

    Updating settings for postgres connection

--- scoring/settings.py
+++ scoring/settings.py
@@ -75,29 +75,29 @@ WSGI_APPLICATION = 'scoring.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': ('postgres'),
-#        'USER': ('scoring'),
-#        'PASSWORD': ('scoring_admin'),
-#        'HOST': 'aa1jor5r4f6buz7.c5qejtxfntrt.us-west-2.rds.amazonaws.com',
-#        'PORT': '',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': ('ebdb'),
+        'USER': ('scoring'),
+        'PASSWORD': ('scoring_admin'),
+        'HOST': 'aamfsl72fiofu5.c5qejtxfntrt.us-west-2.rds.amazonaws.com',
+        'PORT': '',
+    }
+}
 
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': ('postgres'),
+            'NAME': ('ebdb'),
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
