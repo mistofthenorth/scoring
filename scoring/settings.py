8826e0bdf9cdf92fb96af016c591a2d99b610deb
Brian Owen <briano.bend@icloud.com>
2023-06-07 21:18:39 -0700

    updating db name

--- scoring/settings.py
+++ scoring/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'scoring.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
