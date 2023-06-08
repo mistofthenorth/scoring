bc3ee78879a5f879ba39984a77e3c47b4580c11a
Brian Owen <briano.bend@icloud.com>
2023-06-07 21:23:51 -0700

    db name again

--- scoring/settings.py
+++ scoring/settings.py
@@ -87,13 +87,14 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': ('ebdb'),
+            'NAME': ('postgres'),
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
         }
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
